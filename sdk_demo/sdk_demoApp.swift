9309d9255ff79ac90871196bf592ce641377c08c
Austin Jones <austin@alloy.co>
2023-11-30 12:04:00 -0500

    Initial Commit

---
+++ sdk_demo/sdk_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdk_demoApp.swift
+//  sdk_demo
+//
+//  Created by Austin on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct sdk_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
